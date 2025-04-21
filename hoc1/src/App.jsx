c2071c3b0e1fabf75dd5c61fd3bb6d045b50296a
Amaresh Arikatla <amaresh.arikatla@4iapps.com>
2025-04-21 12:11:06 +0530

    initial commit

---
+++ hoc1/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import UserList from './userlist';
+import withLoading from './withloading';
+const UserListWithLoading = withLoading(UserList);
+function App() {
+  const users = [
+    { id: 1, name: 'A' },
+    { id: 2, name: 'B' },
+    { id: 3, name: 'c' },
+  ];
+ 
+  const isLoading = true;
+  return (
+    <div className="App">
+      <h1>My HOC Demo</h1>
+      <UserListWithLoading isLoading={isLoading} users={users} />
+    </div>
+  );
+}
+export default App;
+
+
